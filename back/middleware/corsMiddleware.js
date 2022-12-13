76246ade9068d71c014dc3290181a0529ab79efa
YarL03 <yaroslav.labetsky@yandex.ru>
2022-12-13 23:10:19 +0800

    fix: CORS (allow methods)

--- back/middleware/corsMiddleware.js
+++ back/middleware/corsMiddleware.js
@@ -1,6 +1,6 @@
 export const cors = (req, res, next) => {
     res.set('Access-Control-Allow-Origin', 'https://workout-app-beryl.vercel.app');
     res.set('Access-Control-Allow-Headers', 'origin, content-type, accept, authorization');
-
+    res.set('Access-Control-Allow-Methods', 'PUT')
     next()
 }
